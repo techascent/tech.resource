e66918fb91be5f4fe82d2936e8bd1eec633a6439
Chris Nuernberger <chris@techascent.com>
2018-11-22 13:36:20 -0700

    updating readme.

--- README.md
+++ README.md
@@ -3,24 +3,18 @@
 [![Clojars Project](https://img.shields.io/clojars/v/techascent/tech.resource.svg)](https://clojars.org/techascent/tech.resource)
 
 
-Generic thread-safe and exception-safe resource management.
+Generic thread-safe and exception-safe non-gc or 'off heap' resource management.
 
 
 ```clojure
-(extend-protocol resource/PResource
-  RandomAccessFile
-  (release-resource [^RandomAccessFile item] (.close item)))
-
 ;;create the file
 (defn new-random-file
   [fname]
-  (-> (RandomAccessFile. fname)
-    resource/track))
+  (resource/track (RandomAccessFile. fname) #(.close item)))
 
 
 ;;Use it
-
-(resource/with-resource-context
+(resource/stack-resource-context
   (let [f (new-random-file fname)]
     ...
     ))
@@ -28,8 +22,18 @@ Generic thread-safe and exception-safe resource management.
 ;;Similar to with-open, this will close the file regardless
 ;;of what happens.  The advantage is this can map to anything
 ;;you can implement the resource protocol with meaning network sockets,
-;;JNI pointers, GPU contexts, etc.
+;;JNI pointers, GPU contexts, etc.  It is also possible to have the
+;;gc track something if the dispose functionality does not reference
+;;the item itself.
+
+(let [f (resource/track (double-array [1 2 3]) #(println "disposed") :gc)]
+  ...)
+;;Disposed will print when the gc determins the double array is no longer
+reachable
 ```
+
+Checkout the tests [stack](tests/tech/resource_test.clj) and 
+[gc](tests/tech/gc_resource_tests.clj).
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.  Complements of Tech Ascent, LLC.
--- src/tech/resource.clj
+++ src/tech/resource.clj
@@ -1,4 +1,13 @@
 (ns tech.resource
+  "'off-heap' resource management.  Default management is stack based management but gc
+  or a combination of the two is possible.  To declare a stack region use
+  stack-resource-context.  No matter how the context unwinds, stack resources declared
+  within will be released.
+
+  More options for stack based resources available at tech.resource.stack.
+
+  For GC resources, users must not reference the tracked object in the dispose function
+  else the circular dependency will keep the object in the gc's live set"
   (:require [tech.resource.stack :as stack]
             [tech.resource.gc :as gc]))
 
@@ -12,6 +21,8 @@
 
 
 (defmulti track-impl
+  "Internal implementation to differentiate between different forms of tracking
+  resources."
   (fn [item dispose-fn track-type]
     track-type))
 
@@ -32,18 +43,31 @@
 
 
 (defn track
+  "Track a resource.  If the item inherents from PResource or is a clojure fn, or a
+  Runnable object then it can be cleaned up by the stack system with no further dispose
+  function.  Objects tracked by the gc need to have a dispose fn that does *not*
+  reference the tracked object.
+  Track types can be :gc, :stack, or [:gc :stack]."
   [item & [dispose-fn track-type]]
   (let [track-type (normalize-track-type track-type)]
     (when (and (contains? track-type :gc)
                (not dispose-fn))
-      (throw (ex-info "gc track types must have a dispose function
-that does *not* reference item."
+      (throw (ex-info "gc track types must have a dispose function that does *not*
+reference item."
                       {:item item
                        :track-type track-type})))
     (let [dispose-fn (or dispose-fn item)]
+      (when-not (or (satisfies? stack/PResource dispose-fn)
+                    (fn? dispose-fn)
+                    (instance? Runnable dispose-fn))
+        (throw (ex-info "The dispose method must implement PResource, be Runnable, or a
+clojure function."
+                        {:dispose-fn dispose-fn})))
       (track-impl item dispose-fn track-type))))
 
 
 (defmacro stack-resource-context
+  "Stack resource context.  When this context unwinds, stack resource declared within
+  will be released."
   [& body]
   `(stack/with-resource-context ~@body))
