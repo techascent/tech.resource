f1334bb7295ff306d25278978d2c7dea6c5aeb8d
Chris Nuernberger <chris@thinktopic.com>
2018-12-30 14:27:49 -0700

    Moving to deps.edn where possible.

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://github.com/tech-ascent/tech.resource"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :plugins [[lein-tools-deps "0.4.1"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
   :java-source-paths ["java"])
