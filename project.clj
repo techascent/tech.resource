b5dc9afc71aa7302d8e96db296ce4ed262ee5e41
Chris Nuernberger <chris@techascent.com>
2020-08-19 06:51:42 -0600

    4.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.resource "4.7-SNAPSHOT"
+(defproject techascent/tech.resource "4.7"
   :description "Exception-safe threadsafe resource management"
   :url "http://github.com/tech-ascent/tech.resource"
   :license {:name "Eclipse Public License"
